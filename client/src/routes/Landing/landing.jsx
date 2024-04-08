e57f078383690ceb796d9f765e70da8edb054f76
Turner <doubleujabbour@gmail.com>
2024-04-08 12:52:49 -0400

    update catalog and pictures

--- client/src/routes/Landing/landing.jsx
+++ client/src/routes/Landing/landing.jsx
@@ -5,8 +5,8 @@ export default function Landing() {
     return (
         <div className={styles.landing}>
             <div className={styles.row}>
-                <ClothingIcon img={'/images/F218_black.jpg'} label={`Men's Apparel`} link={'/catalog/mens'} no_space={true}/>
-                <ClothingIcon img={'/images/L407_black.jpg'} label={`Women's Apparel`} link={'/catalog/womens'} no_space={true} />
+                <ClothingIcon img={'/images/J317_black.jpg'} label={`Men's Apparel`} link={'/catalog/mens'} no_space={true}/>
+                <ClothingIcon img={'/images/L317_black.jpg'} label={`Women's Apparel`} link={'/catalog/womens'} no_space={true} />
                 <ClothingIcon img={'/images/TM1MY394_black.jpg'} label={`Accessories`} link={'/catalog/accessories'} no_space={true}/>
             </div>
         </div>
