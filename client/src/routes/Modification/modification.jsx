fecacecdf77a7556b6c5b7407d50e7293b00ad80
Turner <doubleujabbour@gmail.com>
2024-04-10 10:33:56 -0400

    change item order UI

--- client/src/routes/Modification/modification.jsx
+++ client/src/routes/Modification/modification.jsx
@@ -22,19 +22,11 @@ export default function Modification() {
   const [cart, set_cart] = useOutletContext();
   const [selected_color, set_selected_color] = useState(item.default_color);
   const [image_source, set_image_source] = useState(`/images/${item.code}_${selected_color.toLowerCase()}.jpg`)
-  const [quantity_one, set_quantity_one] = useState(0);
-  const [quantity_two, set_quantity_two] = useState(0);
-  const [quantity_three, set_quantity_three] = useState(0);
-  const [color_one, set_color_one] = useState('');
-  const [color_two, set_color_two] = useState('');
-  const [color_three, set_color_three] = useState('');
-  const [size_one, set_size_one] = useState('');
-  const [size_two, set_size_two] = useState('');
-  const [size_three, set_size_three] = useState('');
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarText, setSnackbarText] = useState('Item added to cart')
-  const size_selection = Object.keys(item.sizes)
-  const [selected_size, set_size] = useState(size_selection[0])
+  const sizes = Object.keys(item.sizes)
+  const colors = item.colors
+  const [selected_size, set_size] = useState(sizes[0])
   const [price, set_price] = useState(item.sizes[selected_size])
 
   const color_selection = item.colors.map((color) => {
@@ -53,81 +45,30 @@ export default function Modification() {
   })
 
   function add_item_to_cart() {
-    let item_one, item_two, item_three
-
     const time = Date.now()
     const new_cart = {
       ...cart
     }
-    if (color_one && quantity_one && size_one) {
-      item_one = { name: item.fullname, price: item.sizes[size_one], quantity: quantity_one, size: size_one, color: color_one, code: item.code }
-      new_cart[time] = item_one
-    }
 
-    if (color_two && quantity_two && size_two) {
-      item_two = { name: item.fullname, price: item.sizes[size_two], quantity: quantity_two, size: size_two, color: color_two, code: item.code }
-      new_cart[time + 1] = item_two
+    const table = document.getElementById('table')
+    for (let i = 1; i < table.rows.length; i++) {
+      const inputs = table.rows[i].getElementsByTagName("input")
+      for (let j = 0; j < inputs.length; j++) {
+        if (inputs[j].value) {
+          const cart_item = { name: item.fullname, price: item.sizes[sizes[i-1]], quantity: Number(inputs[j].value), size: sizes[i-1], color: colors[j], code: item.code }
+          new_cart[time + j] = cart_item
+        }
+      }
     }
-
-    if (color_three && quantity_three && size_three) {
-      item_three = { name: item.fullname, price: item.sizes[size_three], quantity: quantity_three, size: size_three, color: color_three, code: item.code }
-      new_cart[time + 2] = item_three
-    }
-
+  
     set_cart(new_cart)
     sessionStorage.setItem('cart', JSON.stringify(new_cart))
     setSnackbarOpen(true)
-
-    set_size_one('')
-    set_size_two('')
-    set_size_three('')
-    set_color_one('')
-    set_color_two('')
-    set_color_three('')
-    set_quantity_one(0)
-    set_quantity_two(0)
-    set_quantity_three(0)
   }
 
   function handleSnackbarClose() {
     setSnackbarOpen(false);
   }
-
-  const handle_quantity_one_change = (event) => {
-    set_quantity_one(event.target.value);
-  };
-
-  const handle_quantity_two_change = (event) => {
-    set_quantity_two(event.target.value);
-  };
-
-  const handle_quantity_three_change = (event) => {
-    set_quantity_three(event.target.value);
-  };
-
-  const handle_color_one = (event) => {
-    set_color_one(event.target.value);
-  };
-
-  const handle_color_two = (event) => {
-    set_color_two(event.target.value);
-  };
-
-  const handle_color_three = (event) => {
-    set_color_three(event.target.value);
-  };
-
-  const handle_size_one = (event) => {
-    set_size_one(event.target.value);
-  };
-
-  const handle_size_two = (event) => {
-    set_size_two(event.target.value);
-  };
-
-  const handle_size_three = (event) => {
-    set_size_three(event.target.value);
-  };
 
   return (
     <div className={styles.container}>
@@ -139,131 +80,32 @@ export default function Modification() {
           <div className={styles.name}>
             {item.fullname}
           </div>
-          <div className={styles.price}>${price} each</div>
-          {item.restricted &&
-            <div className={styles.restriction}>
-              Approved for Service and Parts counters only
-            </div>
-          }
+          <div className={styles.price}>Starts at ${price} each</div>
           <div className={styles.color__selector}>
             {color_selection}
           </div>
           <div className={styles.form__container}>
-            <div className={styles.form}>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Quantity</InputLabel>
-                <Select
-                  value={quantity_one}
-                  onChange={handle_quantity_one_change}
-                >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Color</InputLabel>
-                <Select
-                  id=""
-                  value={color_one}
-                  onChange={handle_color_one}
-                >
-                  {item.colors.map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
+            <table id="table">
+              <thead>
+                <tr>
+                  <th scope="col"></th>
+                  {item.colors.map((color) => {
+                    return <th scope="col">{color}</th>
                   })}
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Size</InputLabel>
-                <Select
-                  id=""
-                  value={size_one}
-                  onChange={handle_size_one}
-                >
-                  {Object.keys(item.sizes).map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-            </div>
-
-            <div className={styles.form}>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Quantity</InputLabel>
-                <Select
-                  value={quantity_two}
-                  onChange={handle_quantity_two_change}
-                >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Color</InputLabel>
-                <Select
-                  id=""
-                  value={color_two}
-                  onChange={handle_color_two}
-                >
-                  {item.colors.map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Size</InputLabel>
-                <Select
-                  id=""
-                  value={size_two}
-                  onChange={handle_size_two}
-                >
-                  {Object.keys(item.sizes).map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-            </div>
-
-            <div className={styles.form}>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Quantity</InputLabel>
-                <Select
-                  value={quantity_three}
-                  onChange={handle_quantity_three_change}
-                >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Color</InputLabel>
-                <Select
-                  id=""
-                  value={color_three}
-                  onChange={handle_color_three}
-                >
-                  {item.colors.map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-              <FormControl id={styles.move} className={styles.text__field} variant="filled" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel>Size</InputLabel>
-                <Select
-                  id=""
-                  value={size_three}
-                  onChange={handle_size_three}
-                >
-                  {Object.keys(item.sizes).map((c) => {
-                    return <MenuItem value={c}>{c}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-            </div>
+                </tr>
+              </thead>
+              <tbody>
+                {sizes.map((size) => {
+                  return <tr>
+                    <th scope="row">{size}</th>
+                    {item.colors.map(() => {
+                      return <td><input type="text" placeholder="0"></input></td>
+                    })}
+                  </tr>
+                })}
+              </tbody>
+            </table>
           </div>
-
 
           <div className={styles.checkout__container} onClick={add_item_to_cart}>
             <p>Add To Cart</p>
