ccad0ee9522814f18990a3961ae024628d4f092c
Turner <doubleujabbour@gmail.com>
2024-04-20 13:58:00 -0700

    css adjustments for footer placement

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ export default function Footer () {
     return (
         <div className={styles.footer}>
             <div className={styles.container}>
+            <h1 className={styles.name}>Guardian Main Line: 800-727-7222</h1>
                 <h1 className={styles.name}>Account Reps: Louis Budbill (678-287-1649) & Glenn Rodney (678-287-1608)</h1>
             </div>
         </div>
--- client/src/routes/Catalog/Catalog.jsx
+++ client/src/routes/Catalog/Catalog.jsx
@@ -34,7 +34,6 @@ export default function Catalog() {
                         )
                     })
                 }
-              
             </div>
         </div>
     )
