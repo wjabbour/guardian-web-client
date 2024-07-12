ccb1eca29c449c1ba5a8a09c10448ba693ba2570
swilliams3722 <swilliams@gpcorp.com>
2024-07-12 08:31:02 -0400

    made changes

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -91,9 +91,9 @@ export const EMBROIDERIES = function () {
   } else if (url.includes('gp-premier.com')) {
     return {
       mens: ['Premier', 'Premhonda','Premnsharvey', 'Premvwharvey', 'Premhyundai', 'Toyotano'],
-      womens: ['Premier', 'Premhonda','Premnsharvey', 'Premvwharvey', 'Premhyundai', 'Toyotano'],
-    }
-  }
+      womens: ['Premier', 'Premhonda','Premnsharvey', 'Premvwharvey', 'Premhyundai', 'Toyotano']
+    } 
+  
 
 export const LOGO_PLACEMENTS = function () {
   const url = window.location.href
