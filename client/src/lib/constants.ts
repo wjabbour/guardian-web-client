be1985d0e600c4305dc0e0dfe5c608e57da952f8
swilliams3722 <swilliams@gpcorp.com>
2024-11-04 09:00:26 -0500

    made changes

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,3 +1,59 @@
+export const STORES = function () {
+  const url = window.location.href
+  if (url.includes('localhost:3000')) {
+    return [
+      'Stivers Ford Montgomery, 4000 Eastern Blvd Montgomery, AL, 36116',
+      'Stivers Ford Montgomery, 500 Palisades Blvd, Birmingham, AL, 35209',
+      'Stivers Hyundai, 9950 Farrow Rd, Columbia, SC, 29203',
+      'Stivers CDJR, 2209 Cobbs Ford Road, Prattville, AL 36066',
+      'Stivers Decatur Subaru, 1950 Orion DR, Decatur, GA 30033',
+      'Stivers Chevrolet, 111 Newland Road, Columbia, SC 29229',
+      'Stivers Ford South Atlanta, 4355 Jonesboro Rd, Union City, GA 30291',
+    ]
+  } else if (url.includes('gpstivers.com')) {
+    return [
+      'Stivers Ford Montgomery, 4000 Eastern Blvd Montgomery, AL, 36116',
+      'Stivers Ford Montgomery, 500 Palisades Blvd, Birmingham, AL, 35209',
+      'Stivers Hyundai, 9950 Farrow Rd, Columbia, SC, 29203',
+      'Stivers CDJR, 2209 Cobbs Ford Rd, Prattville, AL 36066',
+      'Stivers Decatur Subaru, 1950 Orion Dr, Decatur, GA 30033',
+      'Stivers Chevrolet, 111 Newland Road, Columbia, SC 29229',
+      'Stivers Ford South Atlanta, 4355 Jonesboro Rd, Union City, GA 30291',
+    ]
+  } else if (url.includes('gp-------.com')) {
+  return [
+    'Stivers Ford Montgomery, 4000 Eastern Blvd Montgomery, AL, 36116',
+    'Stivers Ford Montgomery, 500 Palisades Blvd, Birmingham, AL, 35209',
+    'Stivers Hyundai, 9950 Farrow Rd, Columbia, SC, 29203',
+    'Stivers CDJR, 2209 Cobbs Ford Road, Prattville, AL 36066',
+    'Stivers Decatur Subaru, 1950 Orion DR, Decatur, GA 30033',
+    'Stivers Chevrolet, 111 Newland Road, Columbia, SC 29229',
+    'Stivers Ford South Atlanta, 4355 Jonesboro Rd, Union City, GA 30291',
+   ]
+  } else if (url.includes('gptameron.com')) {
+    return [
+      'Tameron Honda, 9871 Justina Ave Daphne, AL 36526',
+      'Tameron Buick GMC, 27161 US - 98 Daphne, AL 36526',
+      'Tameron CDJR, 27161 US - 98 Daphne, AL 36526',
+      'Tameron Subaru, 1431 I-65 Service Road Mobile, AL 36606',
+      'Tameron Nissan, 1015 E. I65 Service Road South Mobile, AL 36606',
+      'Tameron Kia, 10611 Boney Ave D, Iberville, MS 39540',
+      'Tameron Kia Westbank, 1884 Westbank Expressway Harvey, LA 70058',
+      'Tameron Honda, 1675 Montgomery Blvd Birmingham, AL 35216',
+      'Tameron Hyundai, 1595 Montgomery Hwy Hoover, AL 35216'
+    ]
+  } else if (url.includes('gp-premier.com')) {
+    return [
+      'Premier Honda, 11801 E I-10 Service Rd, New Orleans, LA 70128',
+      'Toyota of New Orleans, 13150 I-10 Service Rd, New Orleans, LA 70128',
+      'Premier Hyundai of Harvey, 1700 Westbank Expressway, Harvey, LA 70058',
+      'Premier Nissan of Harvey, 4000 LaPalco Boulevard, Harvey, LA 70058',
+      'Premier VW of Harvey, 4050 LaPalco Boulevard, Harvey, LA 70058',
+      'Premier CDJR of Harvey, 1660 Westbrook Expressway, Harvey, LA 70058',
+    ]
+  }
+}
+
 export const KNOWN_CODES = [
   "STIFMO",
   "STIFBI",
@@ -21,5 +77,88 @@ export const KNOWN_CODES = [
   "PRENHA",
   "PREVHA",
   "PRECHB",
-  "PRENME",
-];
+  "PRENME"
+]
+
+export const EMBROIDERIES = function () {
+  const url = window.location.href
+
+  if (url.includes('localhost:3000')) {
+    return {
+      hat: ['Stivers', 'Quicklane', 'Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram'],
+      mens: ['Stivers', 'Quicklane', 'Subaru', 'Stivers Ford South', 'Stivers Ford Birmingham'],
+      womens: ['Stivers', 'Quicklane', 'Subaru', 'Stivers Ford South', 'Stivers Ford Birmingham'],
+      accessory: ['Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram', 'Subaru']
+    }
+  } else if (url.includes('gpstivers.com')) {
+    return {
+      hat: ['Stivers', 'Quicklane', 'Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram'],
+      mens: ['Stivers', 'Quicklane', 'Subaru', 'Stivers Ford South', 'Stivers Ford Birmingham'],
+      womens: ['Stivers', 'Quicklane', 'Subaru', 'Stivers Ford South', 'Stivers Ford Birmingham'],
+      accessory: ['Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram', 'Subaru']
+    }
+  } else if (url.includes('gptameron.com')) {
+    return {
+      mens: ['Tameron'],
+      womens: ['Tameron'],
+      hat: ['Tameron'],
+    }
+  } else if (url.includes('gp---------.com')) {
+    return {
+      hat: ['Stivers', 'Quicklane', 'Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram'],
+      mens: ['Stivers', 'Quicklane', 'Subaru'],
+      womens: ['Stivers', 'Quicklane', 'Subaru'],
+      accessory: ['Ford', 'Hyundai', 'Chrysler', 'Dodge', 'Jeep', 'Ram', 'Subaru']
+    }
+  } else if (url.includes('gp-premier.com')) {
+    return {
+      mens: ['Premier', 'Premier Honda','Premier Nissan Harvey', 'Premier VW Harvey', 'Premier Hyundai', 'Toyota New Orleans', 'Premier Nissan'],
+      womens: ['Premier', 'Premier Honda','Premier Nissan Harvey', 'Premier VW Harvey', 'Premier Hyundai', 'Toyota New Orleans', 'Premier Nissan'],
+    }
+  }
+}
+
+export const LOGO_PLACEMENTS = function () {
+  const url = window.location.href
+  if (url.includes('localhost:3000')) {
+    return ['Right Sleeve', 'Left Chest'];
+  } else if (url.includes('gpstivers.com')) {
+    return ['Right Sleeve', 'Left Chest'];
+  } else if (url.includes('gp---------.com')) {
+    return ['Right Sleeve', 'Left Chest'];
+  } else if (url.includes('gptameron.com')) {
+    return ['Right Sleeve', 'Front'];
+  } else if (url.includes('gp-premier.com')) {
+    return ['Left Chest'];
+  }
+}
+
+export const TITLE = function () {
+  const url = window.location.href
+  if (url.includes('localhost:3000')) {
+    return 'Local';
+  } else if (url.includes('gpstivers.com')) {
+    return 'Stivers';
+  } else if (url.includes('gp--------.com')) {
+    return 'Stivers';
+  } else if (url.includes('gptameron.com')) {
+    return 'Tameron';
+  } else if (url.includes('gp-premier.com')) {
+    return 'Premier';
+  }
+}
+
+export const COMPANY_LOGO = function () {
+  const url = window.location.href
+  if (url.includes('localhost:3000')) {
+    return 'stivers.png';
+  } else if (url.includes('gpstivers.com')) {
+    return 'stivers.png';
+  } else if (url.includes('gp--------.com')) {
+    return 'stivers.png';
+  } else if (url.includes('gptameron.com')) {
+    return 'tameron.png';
+  } else if (url.includes('gp-premier.com')) {
+    return 'premier.png';
+  }
+}
