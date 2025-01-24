277df5cf6e53392befe963ed0e35b010094f7de2
swilliams3722 <swilliams@gpcorp.com>
2025-01-24 08:55:56 -0500

    made changes

--- client/src/lib/catalog.ts
+++ client/src/lib/catalog.ts
@@ -1,13 +1,8 @@
 import { CatalogItem } from "./interfaces";
 import { TameronCatalog } from "../catalogs/tameron";
 import { StiversCatalog } from "../catalogs/stivers";
-import { LeithCatalog } from "../catalogs/leith";
 import { PremierCatalog } from "../catalogs/premier";
-<<<<<<< HEAD
-import { GuardianCatalog } from "../catalogs/guardian";
-=======
 import { NewCustomerCatalog } from "../catalogs/newcustomer";
->>>>>>> faef09e4674ecaf658fbeb8a153917136b750b48
 
 export const Catalog = function (): CatalogItem[] {
   const url = window.location.href;
@@ -16,14 +11,10 @@ export const Catalog = function (): CatalogItem[] {
   } else if (url.includes("newcustomer.gpstivers.com")) {
     return NewCustomerCatalog;
   } else if (url.includes("gpstivers.com")) {
-    return StiversCatalog;
-  } else if (url.includes("gpc81.com")) {
-    return LeithCatalog;
+    return StiversCatalog;  
   } else if (url.includes("gptameron.com")) {
     return TameronCatalog;
   } else if (url.includes("gp-premier.com")) {
-    return PremierCatalog;
-  } else if (url.includes("gpc81.com")) {
-    return GuardianCatalog;  
+    return PremierCatalog; 
   }
 };
