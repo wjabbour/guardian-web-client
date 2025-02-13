a2b462ae18084cf08f6c0ae258c54cf99bbeff8f
Turner <doubleujabbour@gmail.com>
2025-02-12 19:41:55 -0700

    add initial cannon catalog and config

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -2,19 +2,20 @@ import { config as PremierConfig } from "../configs/premier";
 import { config as StiversConfig } from "../configs/stivers";
 import { config as TameronConfig } from "../configs/tameron";
 import { config as NewCustomerConfig } from "../configs/newcustomer";
+import { config as CannonConfig } from "../configs/cannon";
 import { Config } from "./interfaces";
 
 export function getConfigValue(val: keyof Config) {
   const url = window.location.href;
   if (url.includes("localhost:3000")) {
-    return NewCustomerConfig[val];
+    return CannonConfig[val];
   } else if (url.includes("newcustomer.gpstivers.com")) {
     return NewCustomerConfig[val];
   } else if (url.includes("gpstivers.com")) {
-    return StiversConfig[val];  
+    return StiversConfig[val];
   } else if (url.includes("gptameron.com")) {
     return TameronConfig[val];
   } else if (url.includes("gp-premier.com")) {
-    return PremierConfig[val];   
+    return PremierConfig[val];
   }
 }
