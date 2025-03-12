221fc959d33ada258aa16910793ff2e6b67ae0a9
Turner <doubleujabbour@gmail.com>
2025-03-12 08:23:14 -0600

    dont send emails to kristy

--- backend/src/public/configs/cannon.ts
+++ backend/src/public/configs/cannon.ts
@@ -3,7 +3,6 @@ export const cannonConfig: Config = {
   email_recipients: [
     "lbudbill@gpcorp.com",
     "lbudbell@comcast.net",
-    "kburton@nobodybeatsacannondeal.com",
     "doubleujabbour@gmail.com",
   ],
   secretName: "cannon-website",
