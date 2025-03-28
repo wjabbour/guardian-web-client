c75903619a546310cb93451c9dcc81ab3c19d179
swilliams3722 <swilliams@gpcorp.com>
2025-03-28 11:48:54 -0400

    made changes

--- backend/src/public/catalogs/cannon.ts
+++ backend/src/public/catalogs/cannon.ts
@@ -99,7 +99,7 @@ export const CannonCatalog: CatalogItem[] = [
     name: "File Number Labels",
     fullname: "File Right Number Labels (500/box)",
     colors: ["Number 0","Number 1","Number 2","Number 3","Number 4","Number 5","Number 6","Number 7","Number 8","Number 9"],       
-    type: "customs",
+    type: "office",
     default_color: "Number 0",
     discount: [
       { quantity: 500, price: 8.99 },          
