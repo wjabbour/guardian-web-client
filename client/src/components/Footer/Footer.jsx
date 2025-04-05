344a756533c6a5be600e206d22fd2d97c9239229
Turner <doubleujabbour@gmail.com>
2025-04-05 12:48:26 -0600

    restyle footer using tailwind

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import styles from "./Footer.module.scss";
 import { getConfigValue } from "../../lib/config";
 
 export default function Footer() {
@@ -7,10 +6,10 @@ export default function Footer() {
     return `${rep.name} (${rep.phone})`
   }).join(' & ')
   return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-        <h1 className={styles.name}>Guardian Main Line: 800-727-7222</h1>
-        <h1 className={styles.name}>
+    <div className="flex justify-center text-[28px] font-bold">
+      <div className="flex flex-col items-center">
+        <h1>Guardian Main Line: 800-727-7222</h1>
+        <h1>
           Account Reps: {accountRepString}
         </h1>
       </div>
