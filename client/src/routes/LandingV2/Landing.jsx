4fcc822b72c04582798837b7ce139394a08d3e68
Turner <doubleujabbour@gmail.com>
2025-04-05 11:51:31 -0600

    create catalog options on v2 landing

--- client/src/routes/LandingV2/Landing.jsx
+++ client/src/routes/LandingV2/Landing.jsx
@@ -1,21 +1,39 @@
-export default function LandingV2() {
-  function Category(title) {
-    return (
-      <div>
-        <div className="border-solid border-black border h-[375px] w-[300px]"></div>
-        <div className="flex justify-center mt-3">
-          <p className="text-2xl text-[#f54242] font-[700]">{title}</p>
-        </div>
+function Category(props) {
+  return (
+    <div
+      onClick={() => {
+        window.open(props.location, "_blank");
+      }}
+    >
+      <div className="border-solid border-black border h-[375px] w-[300px] cursor-pointer">
+        <img src={props.photo_url} className="h-full w-full"></img>
       </div>
-    );
-  }
+      <div className="flex justify-center mt-3 cursor-default">
+        <p className="text-2xl text-[#f54242] font-[700]">{props.title}</p>
+      </div>
+    </div>
+  );
+}
 
+export default function LandingV2() {
   return (
     <div className="mt-5">
       <div className="flex items-center gap-[105px] justify-center">
-        {Category("Auto Catalog")}
-        {Category("Apparel Catalog")}
-        {Category("Promotional Catalog")}
+        <Category
+          location="https://www.flipsnack.com/gpcorp/guardian-auto-catalog/full-view.html"
+          photo_url="/images/auto.png"
+          title={"Auto Catalog"}
+        ></Category>
+        <Category
+          location="https://www.flipsnack.com/gpcorp/2024apparelcatalog-v7/full-view.html"
+          photo_url="/images/apparel.png"
+          title={"Apparel Catalog"}
+        ></Category>
+        <Category
+          location="https://www.flipsnack.com/gpcorp/24-25-guardian-hit-promo-catalog/full-view.html"
+          photo_url="/images/promotion.png"
+          title={"Promotional Catalog"}
+        ></Category>
       </div>
     </div>
   );
