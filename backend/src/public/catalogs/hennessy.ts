de9a052efc4522b46f8066107c999a58c58dc3a2
swilliams3722 <swilliams@gpcorp.com>
2025-04-17 12:30:36 -0400

    made changes

--- backend/src/public/catalogs/hennessy.ts
+++ backend/src/public/catalogs/hennessy.ts
@@ -34,7 +34,7 @@ export const HennessyCatalog: CatalogItem[] = [
     fullname: "Service Dispatch Numbers (1000/bx)",
     colors: ["0000", "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000"],
     type: "service",
-    default_color: "1000",
+    default_color: "0000",
     discount: [
       { quantity: 1, price: 84.99 },      
     ],
