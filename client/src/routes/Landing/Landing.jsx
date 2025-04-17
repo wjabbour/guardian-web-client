89ef09c51f3845b3a1f44d5503c80e0a9a5b137d
swilliams3722 <swilliams@gpcorp.com>
2025-04-17 10:01:27 -0400

    made changes

--- client/src/routes/Landing/Landing.jsx
+++ client/src/routes/Landing/Landing.jsx
@@ -54,7 +54,7 @@ export default function Landing() {
         )}
         {types["customs"] && (
           <ClothingIcon
-            img={"/images/custom.jpg"}                      
+            img={"/images/custom.png"}                      
             link={"/catalog/customs"}
             no_space={true}
           />
