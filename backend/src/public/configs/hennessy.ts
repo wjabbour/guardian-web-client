fd57c00b7cde3c636faad9523a4731304eabd78a
Turner <doubleujabbour@gmail.com>
2025-05-09 09:41:54 -0600

    remove unverified emails for now

--- backend/src/public/configs/hennessy.ts
+++ backend/src/public/configs/hennessy.ts
@@ -4,7 +4,5 @@ export const hennessyConfig: Config = {
   email_recipients: [
     "lbudbill@gpcorp.com",
     "lbudbell@comcast.net",
-    "asarazua@gpcorp.com",
-    "zwarner@hennessyauto.com",
   ],
 };
