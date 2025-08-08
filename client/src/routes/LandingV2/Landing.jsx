1a88d9c68ca945f44f864480d343c5c8a6e6fa08
Turner <doubleujabbour@gmail.com>
2025-08-07 22:23:44 -0600

    remove test

--- client/src/routes/LandingV2/Landing.jsx
+++ client/src/routes/LandingV2/Landing.jsx
@@ -42,7 +42,6 @@ export default function LandingV2() {
           photo_url="/images/auto.png"
           title={"Auto Catalog"}
         ></Category>
-        asd
         <Category
           location="https://www.flipsnack.com/gpcorp/2024apparelcatalog-v7/full-view.html"
           photo_url="/images/apparel.png"
