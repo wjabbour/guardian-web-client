9f6e344e313c87dce2af8aaa51dccf81f86e743c
swilliams3722 <swilliams@gpcorp.com>
2025-08-14 08:37:26 -0400

    made changes

--- common/interfaces.ts
+++ common/interfaces.ts
@@ -16,7 +16,9 @@ export interface CatalogItem {
     | "detail"
     | "bodyshop"
     | "parts"
+    | "tshirts"
     | "service";
+    
   halfColors?: string[]; // C112 is capable of being two-colored. We need this field to present specially colored divs for the color selector for those colors
   // some items are only purchasble by Service and Parts counter employees only
   restricted?: boolean;
