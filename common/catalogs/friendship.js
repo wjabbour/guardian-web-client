48d0c4051bb78b0210c9577c0695187d822bec4c
swilliams3722 <swilliams@gpcorp.com>
2025-09-24 09:27:44 -0400

    made changes

--- common/catalogs/friendship.js
+++ common/catalogs/friendship.js
@@ -1,6 +1,6 @@
 export const catalog = [
     {
-    code: "FRIENDSHIPFORD-31SD",
+    code: "FRIENDSHIP-FORD-31SD",
     name: "Stroke Dome Decal",
     fullname: "Stroke Dome Decal - chrome w/black imprint (5.75 in. x 1.3 in.)",
     colors: ["Chrome"],
