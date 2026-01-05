b48c590992c7bfc52c155dcbcd88582f0409577e
Shanell <shanell@gpcorp.com>
2026-01-05 15:39:50 -0500

    made changes

--- common/catalogs/mattbowers.js
+++ common/catalogs/mattbowers.js
@@ -1711,6 +1711,21 @@ export const catalog = [
     },
   },
   {
+    code: "BOWNSO-5400-510497Z",
+    name: "All Weather Floor Mats",
+    fullname: "Black All Weather Floor Mats w/full color dome insert",    
+    type: "customs",
+    default_color: "Black",
+    description: `Insert Size 7 in. x 2 in.; Pricing includes shipping`,
+    supportedStores: ["BOWNSO"],
+    quantities: [100],    
+    pricing: {
+      base: {
+        price: 28.80,
+      },
+    },
+  },
+  {
     code: "MATFME-RCM-446180A",
     name: "Metal Key Ring",
     fullname: "Metal Key Ring w/reflex blue imprint",
@@ -2282,22 +2297,7 @@ export const catalog = [
         price: 69.99,
       },
     },
-  },
-  {
-    code: "BOWNSO-5400-510497Z",
-    name: "All Weather Floor Mats",
-    fullname: "Black All Weather Floor Mats w/full color dome insert",    
-    type: "custom",
-    default_color: "Black",
-    description: `Insert Size 7 in. x 2 in.; Pricing includes shipping`,
-    supportedStores: ["BOWNSO"],
-    quantities: [100],    
-    pricing: {
-      base: {
-        price: 28.80,
-      },
-    },
-  },
+  },  
   {
     code: "600088V",
     name: "Seat Covers (roll)",
