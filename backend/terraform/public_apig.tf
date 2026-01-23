bd292990e79f7c48f3bc01bfb5dd53212d8119c8
Turner Jabbour <doubleujabbour@gmail.com>
2026-01-22 22:00:18 -0700

    remove options integration for API routes

--- backend/terraform/public_apig.tf
+++ backend/terraform/public_apig.tf
@@ -22,7 +22,21 @@ resource "aws_api_gateway_deployment" "this" {
       module.me.lambda_function_source_code_hash,
 
       aws_api_gateway_integration.login.id,
-      aws_api_gateway_method.login.id
+      aws_api_gateway_method.login.id,
+      aws_api_gateway_integration.create_order.id,
+      aws_api_gateway_method.create_order.id,
+      aws_api_gateway_integration.update_historical_order.id,
+      aws_api_gateway_method.update_historical_order.id,
+      aws_api_gateway_integration.retrieve_orders.id,
+      aws_api_gateway_method.retrieve_orders.id,
+      aws_api_gateway_integration.capture_order.id,
+      aws_api_gateway_method.capture_order.id,
+      aws_api_gateway_integration.resend_order_email.id,
+      aws_api_gateway_method.resend_order_email.id,
+      aws_api_gateway_integration.delete_order.id,
+      aws_api_gateway_method.delete_order.id,
+      aws_api_gateway_integration.me.id,
+      aws_api_gateway_method.me.id
     ]))
   }
 
@@ -33,13 +47,13 @@ resource "aws_api_gateway_deployment" "this" {
   # Ensure the deployment doesn't happen until the route is fully defined
   depends_on = [
     aws_api_gateway_integration.login,
-    module.create_order_route,
-    module.update_historical_order_route,
-    module.retrieve_orders_route,
-    module.capture_order_route,
-    module.resend_order_email_route,
-    module.delete_order_route,
-    module.me_route
+    aws_api_gateway_integration.create_order,
+    aws_api_gateway_integration.update_historical_order,
+    aws_api_gateway_integration.retrieve_orders,
+    aws_api_gateway_integration.capture_order,
+    aws_api_gateway_integration.resend_order_email,
+    aws_api_gateway_integration.delete_order,
+    aws_api_gateway_integration.me
   ]
 }
 
@@ -49,58 +63,146 @@ resource "aws_api_gateway_stage" "this" {
   stage_name    = "v1"
 }
 
-# --- Standard Modules ---
 
-module "create_order_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+# Create Order Route
+resource "aws_api_gateway_resource" "create_order" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "create-order"
-  uri         = module.create_order.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-module "update_historical_order_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+resource "aws_api_gateway_method" "create_order" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.create_order.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "create_order" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.create_order.id
+  http_method             = aws_api_gateway_method.create_order.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.create_order.lambda_function_invoke_arn
+}
+
+# Update Historical Order Route
+resource "aws_api_gateway_resource" "update_historical_order" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "update-historical-order"
-  uri         = module.update_historical_order.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-module "retrieve_orders_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+resource "aws_api_gateway_method" "update_historical_order" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.update_historical_order.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "update_historical_order" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.update_historical_order.id
+  http_method             = aws_api_gateway_method.update_historical_order.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.update_historical_order.lambda_function_invoke_arn
+}
+
+# Retrieve Orders Route
+resource "aws_api_gateway_resource" "retrieve_orders" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "retrieve-orders"
-  uri         = module.retrieve_orders.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-module "capture_order_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+resource "aws_api_gateway_method" "retrieve_orders" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.retrieve_orders.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "retrieve_orders" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.retrieve_orders.id
+  http_method             = aws_api_gateway_method.retrieve_orders.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.retrieve_orders.lambda_function_invoke_arn
+}
+
+# Capture Order Route
+resource "aws_api_gateway_resource" "capture_order" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "capture-order"
-  uri         = module.capture_order.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-module "resend_order_email_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+resource "aws_api_gateway_method" "capture_order" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.capture_order.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "capture_order" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.capture_order.id
+  http_method             = aws_api_gateway_method.capture_order.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.capture_order.lambda_function_invoke_arn
+}
+
+# Resend Order Email Route
+resource "aws_api_gateway_resource" "resend_order_email" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "resend-order-email"
-  uri         = module.resend_order_email.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-module "delete_order_route" {
-  source      = "./api_gateway_route"
-  http_method = "POST"
+resource "aws_api_gateway_method" "resend_order_email" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.resend_order_email.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "resend_order_email" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.resend_order_email.id
+  http_method             = aws_api_gateway_method.resend_order_email.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.resend_order_email.lambda_function_invoke_arn
+}
+
+# Delete Order Route
+resource "aws_api_gateway_resource" "delete_order" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "delete-order"
-  uri         = module.delete_order.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
 }
 
-# --- MANUAL LOGIN ROUTE (Bypassing Module for Pure Proxy) ---
+resource "aws_api_gateway_method" "delete_order" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.delete_order.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
 
+resource "aws_api_gateway_integration" "delete_order" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.delete_order.id
+  http_method             = aws_api_gateway_method.delete_order.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.delete_order.lambda_function_invoke_arn
+}
+
+# Login Route
 resource "aws_api_gateway_resource" "login" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
@@ -123,12 +225,25 @@ resource "aws_api_gateway_integration" "login" {
   uri                     = module.login.lambda_function_invoke_arn
 }
 
-# --- End Manual Route ---
-
-module "me_route" {
-  source      = "./api_gateway_route"
-  http_method = "GET"
+# Me Route
+resource "aws_api_gateway_resource" "me" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "me"
-  uri         = module.me.lambda_function_invoke_arn
-  api_gateway = aws_api_gateway_rest_api.this
+}
+
+resource "aws_api_gateway_method" "me" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.me.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "me" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.me.id
+  http_method             = aws_api_gateway_method.me.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.me.lambda_function_invoke_arn
 }
