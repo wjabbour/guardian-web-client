d0d0c86ca899fd5205e87ab128b50c83895ed68c
Shanell <shanell@gpcorp.com>
2026-02-26 11:14:16 -0500

    made changes

--- common/configs/tasca.js
+++ common/configs/tasca.js
@@ -2,18 +2,30 @@ export const config = {
   title: "Tasca",
   company_logo: "tasca.png",
   logo_placements: {
-    mens: ["Left Chest", "Right Sleeve","Left Sleeve","Yoke"],
-    womens: ["Left Chest", "Right Sleeve","Left Sleeve","Yoke"],
-    hat: ["Front Center"],
-    tshirts: ["Left Chest", "Right Sleeve"],
+    mens: ["Left Chest"],
+    womens: ["Left Chest"],
+    hat: [],
+    tshirts: [],
   },
   show_image_preview: true,
   minimum_apparel_order: false,
   enable_customs_store_picker: true,
   embroideries: {
     hat: [],
-    mens: ["Autorent.EMB","Tasca.EMB"],
-    womens: ["Autorent.EMB","Tasca.EMB"],
+    mens: [
+      "Tasca Ford", 
+      "Yonkers Kia", 
+      "Scarsdale Ford", 
+      "Central Ave CDJR", 
+      "Gray Crossley Ford"
+    ],
+    womens: [
+      "Tasca Ford", 
+      "Yonkers Kia", 
+      "Scarsdale Ford", 
+      "Central Ave CDJR", 
+      "Gray Crossley Ford"
+    ],
     accessory: [],
     customs: [],
     office: [],
@@ -29,8 +41,193 @@ export const config = {
       address: "1300 Pontiac Ave, Cranston, RI 02920",
       code: "TASFCR",
     },
+    {
+      name: "Tasca Buick GMC Clearwater",
+      address: "18200 US Hwy 19 N, Clearwater,FL 33764",
+      code: "TASBCL",
+    },
+    {
+      name: "Tasca Buick GMC",
+      address: "510 E. Nasa Blvd, Melbourne, FL 32901",
+      code: "TASBME",
+    },
+    {
+      name: "Tasca Buick GMC Palm Harbor",
+      address: "30777 US Hwy 19 N, Palm Harbor, FL 34684",
+      code: "TASBPA",
+    },
+    {
+      name: "Tasca Buick GMC Woonsocket",
+      address: "55 Fortin Drive, Woonsocket, RI 02895",
+      code: "TASBWO",
+    },
+    {
+      name: "Tasca CDJR Fiat",
+      address: "1667 Hartford Ave, Johnston, RI 02919",
+      code: "TASCJO",
+    },
+    {
+      name: "Tasca Cadillac",
+      address: "510 E. Nasa Blvd, Melbourne, FL 32901",
+      code: "TASCME",
+    },
+    {
+      name: "Tasca Chevrolet Wheeling",
+      address: "700 W Dundee Rd, Wheeling, IL 60090",
+      code: "TASCWH",
+    },
+    {
+      name: "Tasca Chevrolet",
+      address: "114 Fortin Drive, Woonsocket, RI 02895",
+      code: "TASCWN",
+    },
+    {
+      name: "Tasca Ford Berlin",
+      address: "250 Webster Square Rd, Berlin, CT 06037",
+      code: "TASFBE",
+    },
+    {
+      name: "Tasca Ford Dickson City",
+      address: "704 Martin F Gibbons Blvd, Dickson City, PA 18519",
+      code: "TASFDI",
+    },
+    {
+      name: "Tasca Ford Seekonk",
+      address: "200 Falls River Ave, Seekonk, MA 02771",
+      code: "TASFSE",
+    },
+    {
+      name: "Tasca Ford Yonkers",
+      address: "900 Central Ave, Yonkers, NY 10704",
+      code: "TASFYO",
+    },
+    {
+      name: "Tasca Centeral Ave Hyundai",
+      address: "111 South Central Avenue, Hartsdale, NY 10530",
+      code: "TASHCE",
+    },
+    {
+      name: "Tasca Hyundai Fairhaven",
+      address: "37 Adlen Rd, Fairhaven, MA 02719",
+      code: "TASHFA",
+    },
+    {
+      name: "Tasca Hyundai Stamford",
+      address: "85 Magee Ave, Stamford, CT 06902",
+      code: "TASHST",
+    },
+    {
+      name: "Tasca Kia",
+      address: "1670 Hartford Ave, Johnston, RI 02919",
+      code: "TASKJO",
+    },
+    {
+      name: "Tasca Kia South Shore",
+      address: "1128 Sunrise Highway, Copiague, NY 11726",
+      code: "TASKSO",
+    },
+    {
+      name: "Tasca Kia White Plains",
+      address: "468 Tarrytown Rd, White Plains, NY 10607",
+      code: "TASKWH",
+    },
+    {
+      name: "Tasca Lincoln",
+      address: "1300 Pontiac Ave, Cranston, RI 02920",
+      code: "TASLCR",
+    },
+    {
+      name: "Tasca Mazda Cranston RI",
+      address: "1300 Pontiac Ave, Cranston, RI 02920",
+      code: "TASMCR",
+    },
+    {
+      name: "Tasca Mazda Seekonk",
+      address: "200 Fall River Ave, Seekonk, MA 02771",
+      code: "TASMSE",
+    },
+    {
+      name: "Tasca Mazda Yonkers",
+      address: "900 Central Park Ave, Yonkers, NY 10704",
+      code: "TASMYO",
+    },
+    {
+      name: "Tasca Subaru Yonkers",
+      address: "900 Central Park Ave, Yonkers, NY 10704",
+      code: "TASSYO",
+    },
+    {
+      name: "Tasca Toyota of Boston",
+      address: "700 Morrissey Blvd, Boston, MA 02122",
+      code: "TASTBO",
+    },
+    {
+      name: "Tasca Volvo Cars",
+      address: "1300 Pontiac Ave, Cranston, RI 02920",
+      code: "TASVCR",
+    },
+    {
+      name: "Tasca VW White Plains",
+      address: "376 Tarrytown Road, White Plains, NY 10607",
+      code: "TASVWP",
+    },
+    {
+      name: "Tasca CDJR Fiat Kingston",
+      address: "129 NY-28, Kingston, NY 12401",
+      code: "TCDJRK",
+    },
+    {
+      name: "Tasca CDJR White Plains",
+      address: "380 Saw Mill River Road, Elmsford, NY 10523",
+      code: "TCDJRW",
+    },
+    {
+      name: "Arlington Heights Ford",
+      address: "801 W Dundee Road, Arlington Heights, IL 60004",
+      code: "ARLFAR",
+    },
+    {
+      name: "Audi Mohegan Lake",
+      address: "1791 E Main St, Mohegan Lake, NY 10547",
+      code: "AUDMOL",
+    },
+    {
+      name: "Audi Meadowlands",
+      address: "4700 West Side Ave, North Bergen, NJ 07047",
+      code: "AUDMEA",
+    },
+    {
+      name: "Central Ave CDJR",
+      address: "1839 Central Ave, Yonkers, NY 10710",
+      code: "CENCYO",
+    },
+    {
+      name: "Scarsdale Ford",
+      address: "887 Central Park Ave, Scarsdale, NY 10583",
+      code: "SCAFSC",
+    },
+    {
+      name: "Gary Crossley Ford",
+      address: "8050 North Church Rd, Kansas City, MO 64158",
+      code: "CROFKA",
+    },
+    {
+      name: "Mohegan Lake VW",
+      address: "1744 East Main Street, Mohegan Lake, NY 10547",
+      code: "MOHVMO",
+    },
+    {
+      name: "Arlington Heights Buick GMC",
+      address: "777 W Dundee Rd, Arlington Heights, IL 60004",
+      code: "TASBAR",
+    },
+    {
+      name: "Yonkers Kia",
+      address: "1850 Central Park Ave, Yonkers, NY 10710",
+      code: "TASKYO",
+    },
   ],
-  bypass_codes: ["TASCA"],
+  bypass_codes: ["TEST"],
   server_hostname: "https://mxfj3q6m01.execute-api.us-east-1.amazonaws.com",
   account_reps: [
     { name: "Louis Budbill", phone: "678-287-1649"}],
